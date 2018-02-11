f7aad2c11f94f805e4ac8f12452478378394a3ff
Matt O'Connell <mattoconnell408@gmail.com>
2018-02-11 15:43:40 -0500

    👽

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react'
+import logo from './logo.png'
+import './App.css'
+
+const jsdiff = require('diff')
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this._configureRecognition()
+    this.state = {
+      recognizing: false,
+      transcript: '',
+      diff: '',
+      answer: 'Esa casa es rojo',
+      submitted: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Audio Differ</h1>
+        </header>
+        <div onClick={this._handleClick} className="socket">
+          <div className={`button ${this.state.recognizing ? 'active': ''}`}></div>
+        </div>
+        <button onClick={this._clear}>Clear</button>
+        <p>Your transcript: { this.state.transcript ? this.state.transcript : '...' }</p>
+        { this.state.submitted &&
+          <div>
+            <p>Correct answer: { this.state.answer }</p>
+            <pre className='diff-result'>
+              {this.state.diff}
+            </pre>
+          </div>
+        }
+      </div>
+    )
+  }
+
+  _handleClick = () => {
+    const hasStarted = this.state.recognizing
+    if (hasStarted) {
+      this.recognition.stop()
+      this.setState({
+        submitted: true
+      })
+    }
+    else {
+      this.recognition.start()
+    }
+    this.setState({
+      recognizing: !hasStarted,
+    })
+  }
+
+  _clear = () => {
+    this.setState({ transcript: '', diff: '', submitted: false })
+  }
+
+  _configureRecognition = () => {
+    this.recognition = new window.webkitSpeechRecognition()
+    this.recognition.continuous = true
+    this.recognition.interimResults = true
+    this.recognition.lang = 'es-NI'
+    this.start_timestamp = ''
+    
+    this.recognition.onstart = () => this.setState({ recognizing: true })
+    
+    this.recognition.onerror = (event) => {
+      if (event.error === 'no-speech') {
+        console.log('info_no_speech')
+      }
+      if (event.error === 'audio-capture') {
+        console.log('info_no_microphone')
+      }
+      if (event.error === 'not-allowed') {
+        if (event.timeStamp - this.start_timestamp < 100) {
+          console.log('info_blocked')
+        } else {
+          console.log('info_denied')
+        }
+      }
+    }
+
+    this.recognition.onend = () => {
+      console.log('on end')
+      this.setState({ recognizing: false })
+    }
+
+    this.recognition.onresult = (event) => {
+      var interim_transcript = ''
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          this.setState({ transcript: this.state.transcript += event.results[i][0].transcript })
+        } else {
+          console.log('not final')
+          interim_transcript += event.results[i][0].transcript
+        }
+      }
+      this.setState({
+        diff: this._diffTranscript(this.state.transcript)
+      })
+    }
+  }
+
+  _diffTranscript(newTranscript, comparison = this.state.answer) {
+    return jsdiff.diffChars(newTranscript.toLowerCase(), comparison.toLowerCase()).map(function(part, index) {
+      var spanStyle = {
+        backgroundColor: part.added ? 'lightgreen' : part.removed ? 'salmon' : ''
+      };
+      return <span key={index} style={spanStyle}>{part.value}</span>
+    });
+  }
+}
+
+export default App
